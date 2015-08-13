c58ab1b927e03b0107517d368b424f67f935b32e
PugCoder <pugcoder@icloud.com>
2015-08-13 12:45:46 -0500

    Initial Commit

---
+++ GestureKit/GestureKit.h
@@ -0,0 +1,19 @@
+//
+//  GestureKit.h
+//  GestureKit
+//
+//  Created by Jieyi Hu on 8/13/15.
+//  Copyright © 2015 SenseWatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GestureKit.
+FOUNDATION_EXPORT double GestureKitVersionNumber;
+
+//! Project version string for GestureKit.
+FOUNDATION_EXPORT const unsigned char GestureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GestureKit/PublicHeader.h>
+
+
---
+++ GestureKitTests/GestureKitTests.swift
@@ -0,0 +1,35 @@
+//
+//  GestureKitTests.swift
+//  GestureKitTests
+//
+//  Created by Jieyi Hu on 8/13/15.
+//  Copyright © 2015 SenseWatch. All rights reserved.
+//
+
+import XCTest
+
+class GestureKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
